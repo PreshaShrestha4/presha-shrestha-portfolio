069a3a5851d1191d00659dbc3279d46e6569ee83
PreshaShrestha4 <preshashrestha041@gmail.com>
2026-02-19 18:57:33 +0545

    updated

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const navLinks = document.getElementById("navLinks");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+const techBoxes = document.querySelectorAll(".tech-box");
+
+techBoxes.forEach((box) => {
+  const color = box.getAttribute("data-color");
+  box.style.background = `linear-gradient(135deg, ${color}CC 0%, ${color}99 100%)`;
+});
